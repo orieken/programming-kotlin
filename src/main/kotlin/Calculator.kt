1a35bcf7f497d9e433e252b102ef28ab8626f8b9
Rieken <oscar.rieken@adp.com>
2019-09-10 23:40:34 -0400

    chore(lint): fixing lint errors 😅

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,5 +1,5 @@
 class Calculator {
-    fun add(a: Int, b: Int): Int {
-        return a + b
-    }
+  fun add(a: Int, b: Int): Int {
+    return a + b
+  }
 }
--- src/test/kotlin/CalculatorTests.kt
+++ src/test/kotlin/CalculatorTests.kt
@@ -1,17 +1,17 @@
-import org.junit.jupiter.api.Test
-
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.Is.`is`
+import org.junit.jupiter.api.Test
 
 class CalculatorTests {
-    @Test
-    fun `1 + 1 = 2`() {
-        val calculator = Calculator()
-        assertThat(calculator.add(1,1), `is`(2))
-    }
-    @Test
-    fun `10 + 10 = 20`() {
-        val calculator = Calculator()
-        assertThat(calculator.add(10,10), `is`(20))
-    }
+  @Test
+  fun `1 + 1 = 2`() {
+    val calculator = Calculator()
+    assertThat(calculator.add(1, 1), `is`(2))
+  }
+
+  @Test
+  fun `10 + 10 = 20`() {
+    val calculator = Calculator()
+    assertThat(calculator.add(10, 10), `is`(20))
+  }
 }
